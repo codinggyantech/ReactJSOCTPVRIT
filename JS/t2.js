bf1700cdb0b6a0b40c0717b96f7962d56b0eb333
CodingGyanTech <codinggyantech@gmail.com>
2023-11-06 18:43:14 +0530

    console promt alert and letvar

---
+++ JS/t2.js
@@ -0,0 +1,50 @@
+console.log("Variable in JS ")
+
+
+a = 'bablu'
+// to write variable we have to foloow some rules 
+
+
+balbu = 2345
+
+// 5var = 23  no numbers at start
+// bablu kumar = 23 no spaces 
+// @ba = 23  no special characters  but _ is exceptional
+
+// we cannot use keywords as variable 
+
+a = 23
+A= 54
+
+balbu_kumar = 23
+bablu23 = 23;
+// console.log(balbu_kumar)
+
+//  commments in  js  developers notes 
+
+/**
+ * Multi line comments in 
+ * JS 
+ */
+
+
+
+// Data types in JS  
+
+console.log('Data types in JS')
+a =  'ram'; 
+a = 23
+a = true
+a = false 
+a = 23.445
+a = "Shyam"
+
+ c = null
+
+
+ document.write("hello i am comming form js code")
+
+//  console.log(c, typeof(c))
+console.log(typeof(b))
+
+console.log( a ,typeof(a))
---
+++ JS/t3.js
@@ -0,0 +1,77 @@
+console.log('t3 connecteed')
+
+
+// console log function 
+
+// console.log('Logging | prininting')
+// console.info('console info')
+
+// console.warn('waringingg')
+
+// console.error('respresnt errror')
+
+
+// a = [1, 2, 3, 5, 89, 4676, 32478, 367];
+// console.table(a)
+
+
+/// alerts in js : 
+// alert('Alert in JS')
+// alert('testng alerts ')
+
+// prompts in js : 
+
+// to take the users input 
+// a = prompt('enter your name')
+// b = prompt('Enter your age')
+// console.log(a,typeof(a))
+
+
+// console.log(a,typeof(a))
+
+// b = parseInt(b)
+// console.log(b,typeof(b))
+// b = String(b)
+// console.log(b,typeof(b))
+
+
+// the process to convert one datatype to another is called as typecasting 
+
+// console.log('your name is',a,' and your age is',b)
+
+
+
+// confim box in js 
+ 
+
+// a = confirm("Are you sure you want to continue")
+// console.log(a)
+
+
+// if(a){
+//     console.log("order placed success")
+// }
+// else{
+//     console.log('order cancelled')
+// }
+
+
+
+// var a = 23
+// var a = 44
+// let a = 34
+//  a = 35
+//  a = 'ram'
+//  a = true
+
+
+//  const b  = 3.14
+
+//  b = 23
+
+
+
+
+// console.log(a)
+
+// console.log(b)
