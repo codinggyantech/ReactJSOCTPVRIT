5ae3edb2c7d72ea8f14dbd6c091c1382e1cc5607
CodingGyanTech <codinggyantech@gmail.com>
2023-11-10 18:41:44 +0530

    t7 loops and error handling in js

---
+++ JS/t7.js
@@ -0,0 +1,87 @@
+console.log('Learning Loops in JS')
+
+
+//  Hey i need number 1-5 printed in console 
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+//Hey 1-5 but we need 1-100 now : 
+
+// USE: to do repetative task 
+// to save the line of code  
+// to save my time 
+
+// HOw many types: LOOPS
+
+// 3 kind of loops: 
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+
+// 1. for loop 
+
+/**
+ * syntax of for loop : 
+ * 
+ *  for (initiatilzation ; codintion ; inc/dec){
+ * // body of the for loop 
+ * 
+ * }
+ */
+
+
+
+// for(i = 0; i<=5 ; i++){
+//     console.log(i)
+// }
+
+// for(a = 100; a>=0;a--){
+//     console.log(a)
+// }
+// a = [23,56,3456,3446,23556,367,346,'ram']
+// console.log(a)
+
+// for(i = 0; i<a.length;i++){
+//  console.log(a[i])
+// }
+
+ 
+
+// while 
+
+// let i = 1
+
+// while (i<-1) {
+    
+// console.log(i)
+// i++
+
+// }
+
+
+
+// let j = 1
+// do {
+//     console.log(j)
+//     j++
+// } while (j<-1);
+
+
+// error handiline or execption handling in js 
+ 
+a = 2/'hello'
+console.log(rameshwar)
+
+try {
+
+    a = 20/0 
+    console.log(abgfvf)
+    
+} catch (error) {
+    console.log(error)
+    
+}
