62c2db430ae637eff8cfc275f59242f6f7402fb5
CodingGyanTech <codinggyantech@gmail.com>
2023-11-14 18:40:19 +0530

    added t9  with js and adv concept

---
+++ JS/t9.js
@@ -0,0 +1,21 @@
+console.log('MODULES IN JS ')
+
+console.log(2+3)
+
+
+console.log('math rouund fun',Math.round(3.88))
+console.log('math rouund fun',Math.round(3.4))
+
+console.log(Math.floor(3.99),Math.ceil(1.01))
+
+maxnum = Math.min(3,677,9,54,99,2)
+maxnum = Math.max(3,677,9,54,99,2)
+
+Math.E
+// Math.max()
+console.log(maxnum)
+
+
+// We have so many functions related to Date. 
+d = new Date()
+console.log(d)
