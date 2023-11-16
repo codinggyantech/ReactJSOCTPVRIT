7e4c29ef5a248af622380384e51ad289e022e240
CodingGyanTech <codinggyantech@gmail.com>
2023-11-16 18:50:32 +0530

    react js first app creation

---
+++ ReactJS/my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello with REACT !</h1>
+        <p>
+          
+          CODING GYAN TECH
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learning React JS In a FUN way
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
