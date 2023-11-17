646e6bcabafd3df6d62a2648fe37dea6680b871d
CodingGyanTech <codinggyantech@gmail.com>
2023-11-17 18:41:50 +0530

    react js with helloworld components and folder str

---
+++ ReactJS/mycomponent/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+const Home = ()=>  <h2>I am a Home function Update</h2>
+
+
+
+
+function NavBar(){
+  return <h1> You are a Navbar</h1>
+}
+
+
+
+function App() {
+  return (
+    <>
+    <p>wrting in App</p>
+    <NavBar></NavBar>
+    <Home></Home>
+    <Home></Home>
+    <Home></Home>
+    <Home></Home>
+    <Home></Home>
+    <Home></Home>
+
+    
+    </>
+  );
+}
+
+export default App;
